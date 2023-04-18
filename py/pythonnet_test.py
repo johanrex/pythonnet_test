41fa642ff396532e15078f6beebe135da823cb39
Johan Rex <serexj@tetrapak.com>
2023-04-18 09:37:23 +0200

    whitespace

--- py/pythonnet_test.py
+++ py/pythonnet_test.py
@@ -1,19 +1,19 @@
 # pip install pythonnet
 
+
 def small_test():
-    from pythonnet import load    
+    from pythonnet import load
+
     runtimeconfig_path = r"C:\git\pythonnet_test\cs\testlib\out\testlib.runtimeconfig.json"
     load("coreclr", runtime_config=runtimeconfig_path)
 
     import clr
     import sys
 
-
     sys.path.append(r"C:\git\pythonnet_test\cs\testlib\out")
-    
     clr.AddReference("testlib")
-    from testlib import Class1
 
+    from testlib import Class1
     n = Class1().CountDevices()
 
     print(f"Found {n} devices")
